54f887e4d6407ece6c589f5d8d6ab113a630bce2
leonahpd <lionaraj97@gmail.com>
2025-11-20 13:02:29 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -21,8 +21,3 @@ provider "azurerm" {
 
 }
 
-variable "resource_group_name" {
-  type        = string
-  default     = "AKS-resource"
-  description = "Resource group name in your Azure subscription."
-}
