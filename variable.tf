5cde79529816ca9c207094cbc3f777146e601d46
leonahpd <lionaraj97@gmail.com>
2025-11-20 13:04:09 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -3,3 +3,14 @@ variable "resource_group_name" {
   default     = "AKS-resource"
   description = "Resource group name in your Azure subscription."
 }
+
+#variable "client_id" {
+#  description = "Service Principal Client ID for AKS"
+#  type        = string
+#}
+#
+#variable "client_secret" {
+#  description = "Service Principal Client Secret for AKS"
+#  type        = string
+#  sensitive   = true
+#}
