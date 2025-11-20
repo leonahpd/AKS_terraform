04ba0997ce9e07e38a16d7c3c1663fdd8a5c9558
leonahpd <lionaraj97@gmail.com>
2025-11-20 13:12:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -164,15 +164,3 @@ resource "azurerm_kubernetes_cluster" "kubernetes_aks2" {
 }  
 
 
-output "management_client_certificate" {
-  value     = azurerm_kubernetes_cluster.kubernetes_aks2.kube_config[0].client_certificate
-  sensitive = true
-}
-
-output "management_kube_config" {
-  value = azurerm_kubernetes_cluster.kubernetes_aks2.kube_config_raw
-
-  sensitive = true
-}
-
-
